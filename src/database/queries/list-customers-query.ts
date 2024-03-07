459cc811c57f1f4142c3c9e0aa82d3085663441e
gitirana <thaygitirana@gmail.com>
2024-03-07 14:07:51 -0300

    feat: create customers list filters

--- src/database/queries/list-customers-query.ts
+++ src/database/queries/list-customers-query.ts
@@ -1,9 +1,48 @@
 import { query } from '..'
 
-export async function listCustomersQuery() {
-  const sql = `
-    SELECT * FROM customers 
+interface listCustomersFilters {
+  page: number
+  name?: string
+  email?: string
+  phone?: string
+}
+
+export async function listCustomersQuery({
+  page,
+  email,
+  name,
+  phone,
+}: listCustomersFilters) {
+  const limit = 1
+  const offset = (page - 1) * limit
+  const pageNumber = page ? parseInt(page.toString(), 10) : 1
+
+  if (isNaN(pageNumber)) {
+    return
+  }
+
+  let sql = `
+    SELECT * FROM customers WHERE 1 = 1
   `
+
+  const conditions = []
+
+  if (name) {
+    conditions.push(`name ILIKE '%${name}%'`)
+  }
+  if (email) {
+    conditions.push(`email ILIKE '%${email}%'`)
+  }
+  if (phone) {
+    conditions.push(`phone ILIKE '%${phone}%'`)
+  }
+  if (conditions.length > 0) {
+    sql += ' AND ' + conditions.join(' AND ')
+  }
+
+  sql += ' ORDER BY created_at DESC'
+  sql += ` LIMIT ${limit} OFFSET ${offset}`
+
   const customers = await query(sql)
 
   return customers
--- src/http/routes/list-customers.ts
+++ src/http/routes/list-customers.ts
@@ -1,10 +1,27 @@
+import { z } from 'zod'
+
 import { listCustomersQuery } from '../../database/queries/list-customers-query'
 import { app } from '../app'
 
 export async function listCustomers() {
-  app.get('/customers', async () => {
-    const customers = await listCustomersQuery()
+  app.get('/customers', async (request, reply) => {
+    const listCustomersHeaderSchema = z.object({
+      page: z.coerce.number().default(1).optional(),
+      name: z.string().optional(),
+      email: z.string().optional(),
+      phone: z.string().optional(),
+    })
+    const { email, name, page, phone } = listCustomersHeaderSchema.parse(
+      request.query,
+    )
 
-    return customers
+    const customers = await listCustomersQuery({
+      page: page || 1,
+      name,
+      email,
+      phone,
+    })
+
+    reply.status(200).send(customers)
   })
 }
