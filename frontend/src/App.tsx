5d85fff94a4f76c8c21a36d5085c64eeb1c5e1a8
gitirana <thaygitirana@gmail.com>
2024-03-07 16:47:13 -0300

    feat: create header and theme toggle

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,20 @@
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 
+import { ThemeProvider } from './components/theme/theme-provider'
 import { router } from './routes'
 
 export function App() {
   return (
     <>
       <HelmetProvider>
-        <Helmet titleTemplate="%s | challenge" />
-        <RouterProvider router={router}></RouterProvider>
+        <ThemeProvider
+          storageKey="customer-challenge:theme"
+          defaultTheme="dark"
+        >
+          <Helmet titleTemplate="%s | challenge" />
+          <RouterProvider router={router}></RouterProvider>
+        </ThemeProvider>
       </HelmetProvider>
     </>
   )
